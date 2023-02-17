09ff1834f391b973555bdb74fdff0469f47f3ae0
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2023-02-17 21:07:23 +0300

    Сделала layout элемента списка размещений

---
+++ app/src/main/java/com/oneseed/studtourism/ui/search/AccommodationAdapter.kt
@@ -0,0 +1,23 @@
+package com.oneseed.studtourism.ui.search
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AccommodationAdapter: RecyclerView.Adapter<AccommodationAdapter.AccomodationHolder>() {
+    class AccomodationHolder(item: View) : RecyclerView.ViewHolder(item) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccomodationHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AccomodationHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
