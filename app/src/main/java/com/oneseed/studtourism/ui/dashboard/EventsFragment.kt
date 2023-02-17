9ea4822dc4befc8b355899719afac9144188e0e8
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2023-02-17 17:08:28 +0300

    Поменяла название фрагмента Dashboard на Events

--- app/src/main/java/com/oneseed/studtourism/ui/dashboard/EventsFragment.kt
+++ app/src/main/java/com/oneseed/studtourism/ui/dashboard/EventsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.oneseed.studtourism.databinding.FragmentDashboardBinding
+import com.oneseed.studtourism.databinding.FragmentEventsBinding
 
-class DashboardFragment : Fragment() {
+class EventsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentEventsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentEventsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
